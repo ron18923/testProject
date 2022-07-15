e559943427fc36d84b5107895800def622fdb612
ron18923 <ronron18923@gmail.com>
2022-07-15 17:36:40 +0300

    fixing show_trades not working on the graph

--- calculations.py
+++ calculations.py
@@ -4,15 +4,15 @@ import math
 import numpy as np
 
 
-def manual_similarity_measure(*argv):
-    # input type: [x,y]
-
-    denominator = len(argv)
-
-    max_value = np.amax(argv)
+def manual_similarity_measure(first, second):
 
     numerator = 0
-    for arg in argv:
+    max_value = np.maximum(first, second)
+
+    shorter = max(first, second)
+    denominator = len(shorter)
+
+    for index in range(shorter):
         numerator += (arg[0]/max_value-arg[1]/max_value)**2
 
     return 1 - math.sqrt(numerator/denominator)
--- main.py
+++ main.py
@@ -51,57 +51,6 @@ def order_by_size(num1, num2):
     return list
 
 
-# def similarity_between_two_candles(first_candle, second_candle):
-#     open_list = []
-#     close_list = []
-#     high_list = []
-#     low_list = []
-#     # based on: https://developers.google.com/machine-learning/clustering/similarity/manual-similarity
-#     open_list.append(first_candle["open"])
-#     close_list.append(first_candle["close"])
-#     high_list.append(first_candle["high"])
-#     low_list.append(first_candle["low"])
-#
-#     open_list.append(second_candle["open"])
-#     close_list.append(second_candle["close"])
-#     high_list.append(second_candle["high"])
-#     low_list.append(second_candle["low"])
-#
-#     open_list.sort(reverse=True)
-#     close_list.sort(reverse=True)
-#     high_list.sort(reverse=True)
-#     low_list.sort(reverse=True)
-#
-#     open_diff_pow = (1 - (open_list.__getitem__(len(open_list) - 1) / open_list.__getitem__(0))) ** 2
-#     close_diff_pow = (1 - (close_list.__getitem__(len(close_list) - 1) / close_list.__getitem__(0))) ** 2
-#     high_diff_pow = (1 - (high_list.__getitem__(len(high_list) - 1) / high_list.__getitem__(0))) ** 2
-#     low_diff_pow = (1 - (low_list.__getitem__(len(low_list) - 1) / low_list.__getitem__(0))) ** 2
-#
-#     return 1 - math.sqrt((open_diff_pow + close_diff_pow + high_diff_pow + low_diff_pow) / 4)
-#     # Todo check if dividing in integer makes the float number into integer
-
-
-# def similarity_between_four_candles(list_candles):
-#     if len(list_candles) != 4:
-#         logging.warning("list should include 4 candles!")
-#         return
-#
-#     open_list = []
-#     close_list = []
-#     high_list = []
-#     low_list = []
-#     # based on: https://developers.google.com/machine-learning/clustering/similarity/manual-similarity
-#     for candle in list_candles:
-#         open_list.append(candle["open"])
-#         close_list.append(candle["close"])
-#         high_list.append(candle["high"])
-#         low_list.append(candle["low"])
-#
-#     open_list.sort(reverse=True)
-#     close_list.sort(reverse=True)
-#     high_list.sort(reverse=True)
-#     low_list.sort(reverse=True)
-
 def similarity_between_two_candles(first_candle, second_candle):
     open_list = []
     close_list = []
@@ -162,27 +111,4 @@ def trading_strategy(gemini: Gemini, data):
 if __name__ == '__main__':
     data_df = poloniex.load_dataframe(pair=PAIR, period=PERIOD, days_history=DAYS_HISTORY)
 
-    print(data_df["close"], (0, 10), (10, 20))
-
-    # print(cl.manual_similarity_measure([98.19, 99.64], [98.32, 99.46], [98.43, 99.39], [98.41, 99.40], [98.57, 99.34],
-    #                                   [98.71, 99.33], [98.66, 99.34], [98.72, 99.39], [98.80, 99.34], [98.70, 99.24]))
-    # down below are different trending chunks of charts
-    # print(cl.manual_similarity_measure([-.02, .07], [.13, -.18], [.11, -.07], [-.02, .01], [.16, -.06], [.14, -.01],
-    #                                    [-.05, .01], [.06, .05], [.08, -.05], [-.10, -.10]))
-    # # down below are kinda simillar chunks of charts
-    # print(cl.manual_similarity_measure([-.02, .04], [.13, .13], [.11, .16], [-.02, .06], [.16, -.14], [.14, 18],
-    #                                    [-.05, -.03], [.06, -.08], [.08, .04], [-.10, .01]))
-
-
-
-# last_candle = data_df.iloc[len(data_df) - 1]
-# second_last_candle = data_df.iloc[len(data_df) - 200]
-#
-# similarity_between_two_candles(last_candle, second_last_candle)
-
-# first_section = data_df.iloc[0:10]
-# second_section = data_df.iloc[28000:28010]
-# similarity_two_sections(first_section, second_section)
-
-# backtesting_engine = Gemini(logic=trading_strategy, sim_params=params, analyze=analyze.analyze_bokeh)
-# backtesting_engine.run(data=data_df)
+    print(data_df["close"][len(data_df)-20: len(data_df)-10], data_df["close"][len(data_df)-10:])
