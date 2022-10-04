68f0dc39cc38f4ab62aaf6aca0879627e5a0fa65
ron18923 <ronron18923@gmail.com>
2022-10-05 01:40:34 +0300

    - planning on trying new method, so I am abandoning this project and trying again.

--- chart_data.py
+++ chart_data.py
@@ -6,66 +6,18 @@ import pandas as pd
 import requests
 
 
-def get_data_from_poloniex(params):
+def get_data_from_poloniex(pair, start, end, period):
+    params = {
+        'command': 'returnChartData',
+        'currencyPair': pair,
+        'start': start,
+        'end': end,
+        'period': period
+    }
+
     response = requests.get('https://poloniex.com/public', params=params)
     data = response.json()
     return data
-
-
-def get_chart(pair, period, days_history=30):
-    """
-    Returns historical charts data from poloniex.com
-    :param pair: the pair to get historical data of
-    :param period: the intervals between each point
-    :param days_history: from when to get the data
-    :return: pandas.dataframe of the pair's historical data
-    """
-
-    df = pd.DataFrame()
-    items_per_iteration = 100
-
-    # counting how much items we need to get (24 * 60 * 60 * days_history) / period
-    # counting how much times the for loop needs to run int(items_amount/100) + (1 if items_amount%100 > 0 else 0)
-    # rounding to last time stamp: int(time.time()/period)*period
-    items_amount = (24 * 60 * 60 * days_history) / period
-    loop_amount = int((items_amount / items_per_iteration) + (1 if items_amount % items_per_iteration > 0 else 0))
-    rounded_timestamp = int(time.time() / period) * period
-    for index in range(loop_amount):
-        if index != loop_amount - 1:
-            start = rounded_timestamp - (items_per_iteration - 1) * period
-            end = rounded_timestamp
-            rounded_timestamp = rounded_timestamp - (items_per_iteration - 1) * period
-
-        else:
-            items_left_to_get = (items_amount % items_per_iteration)
-            if items_left_to_get == 0:
-                continue
-
-            start = rounded_timestamp - (items_left_to_get - 1) * period
-            end = rounded_timestamp
-            rounded_timestamp = rounded_timestamp - (items_amount % items_per_iteration - 1) * period
-
-        params = {
-            'command': 'returnChartData',
-            'currencyPair': pair,
-            'start': start,
-            'end': end,
-            'period': period
-        }
-
-        data = get_data_from_poloniex(params)
-
-        try:
-            df = pd.concat([df, pd.DataFrame(data)])
-        except ValueError:
-            response = requests.get('https://poloniex.com/public', params=params)
-            data = response.json()
-            df = pd.concat([df, pd.DataFrame(data)])
-
-    df = df.sort_values(by=['date'])
-    df['date'] = pd.to_datetime(df['date'], unit='ms')  # pandas default is
-    df = df.set_index(['date'])
-    return df
 
 
 def load_save_df(pair, period, save= None):
@@ -82,6 +34,13 @@ def load_save_df(pair, period, save= None):
         return df
     else:
         save.to_pickle(file_path)
+
+
+def make_df_by_date(df):
+    df = df.sort_values(by=['date'])
+    df['date'] = pd.to_datetime(df['date'], unit='ms')  # pandas default is
+    df = df.set_index(['date'])
+    return df
 
 
 def get_data(pair, period, days_history=30):
@@ -121,26 +80,15 @@ def get_data(pair, period, days_history=30):
             end = rounded_timestamp
             rounded_timestamp = rounded_timestamp - (items_amount % items_per_iteration - 1) * period
 
-        params = {
-            'command': 'returnChartData',
-            'currencyPair': pair,
-            'start': start,
-            'end': end,
-            'period': period
-        }
-
-        data = get_data_from_poloniex(params)
+        data = get_data_from_poloniex(pair, start, end, period)
 
         try:
             df_to_add = pd.concat([df_to_add, pd.DataFrame(data)])
         except ValueError:
-            response = requests.get('https://poloniex.com/public', params=params)
-            data = response.json()
+            data = get_data_from_poloniex(pair, start, end, period)
             df_to_add = pd.concat([df_to_add, pd.DataFrame(data)])
 
-    df_to_add = df_to_add.sort_values(by=['date'])
-    df_to_add['date'] = pd.to_datetime(df_to_add['date'], unit='ms')  # pandas default is
-    df_to_add = df_to_add.set_index(['date'])
+    df_to_add = make_df_by_date(df_to_add)
     df = pd.concat([df, df_to_add])
 
     saved_df = pd.concat([saved_df, df_to_add])
--- main.py
+++ main.py
@@ -16,9 +16,9 @@ PERIOD ALLOWED VALUES:
 """
 PERIOD = 300
 PAIR = "USDT_BTC"
-DAYS_HISTORY = 10  # oldest date 08/10/2021(dd/mm/yyyy)
+DAYS_HISTORY = 1  # oldest date 08/10/2021(dd/mm/yyyy)
 
-COMPARISON_LENGTH = 70
+COMPARISON_LENGTH = 10
 
 
 def max_elements(df, comparison_length):
@@ -30,9 +30,29 @@ def max_elements(df, comparison_length):
         close = df['close'][len(df) - j - comparison_length]
         list = list.append({'accuracy': accuracy, 'date': date, 'close': close}, ignore_index=True)
     list = list.sort_values(by=['accuracy'])
-    print(list)
-
+    return list
 
 if __name__ == '__main__':
     data_df = chart_data.get_data(pair=PAIR, period=PERIOD, days_history=DAYS_HISTORY)
     max_elements(data_df, COMPARISON_LENGTH)
+
+    # x axis values
+    x = [1,2,3]
+    # corresponding y axis values
+    y = [2,4,1]
+
+    # plotting the points
+    plt.plot(x, y)
+
+    # naming the x axis
+    plt.xlabel('x - axis')
+    # naming the y axis
+    plt.ylabel('y - axis')
+
+    # giving a title to my graph
+    plt.title('My first graph!')
+
+    # function to show the plot
+    plt.show()
+    pass
+
